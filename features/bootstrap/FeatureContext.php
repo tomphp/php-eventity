eff54499176c4485393b8be38e45fecb6f1a6eda
Tom Oram <tom@x2k.co.uk>
2015-08-24 09:51:42 +0100

    Add new snippets for the next feature

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -11,6 +11,7 @@ use Eventity\ClassDefinition\ClassCodeRenderer;
 use Eventity\Event;
 use Eventity\ClassDefinition\ClassDeclarer;
 use Eventity\Eventity;
+use Eventity\ClassDefinition\MethodDefinition;
 
 class FeatureContext implements Context, SnippetAcceptingContext
 {
@@ -45,6 +46,31 @@ class FeatureContext implements Context, SnippetAcceptingContext
     }
 
     /**
+     * @Given there is an entity class named :className with a method called :action
+     */
+    public function thereIsAnEntityClassNamedTestentityWithAMethodCalled($className, $action)
+    {
+        $builder = new ClassDefinitionBuilder($this->createTestClassName($className));
+
+        $builder->addMethod(MethodDefinition::createPublic(
+            $action,
+            "if (\$this->calls['$action']) \$this->calls['$action'] = 0;\n"
+            . "\$this->calls['$action']++;"
+        ));
+
+        $builder->addProperty(Scope::publicScope(), 'calls', Value::emptyArray());
+
+        $builder->addMethod(MethodDefinition::createPulicWithArgs(
+            'getCalls',
+            ['methodName'],
+            'return isset($this->calls[$methodName]) ? $this->calls[$methodName] : 0;'
+        ));
+
+        (new ClassDeclarer(new ClassCodeRenderer()))->declareClass($builder->build());
+    }
+
+    /**
+     * @Given I have created an instance of :className with the factory
      * @When I create an instance with the factory for :className
      */
     public function iCreateAnInstanceWithTheFactoryFor($className)
@@ -55,23 +81,41 @@ class FeatureContext implements Context, SnippetAcceptingContext
     }
 
     /**
+     * @When I call :action on then instance
+     */
+    public function iCallOnThenInstance($action)
+    {
+        $this->instance->$action();
+    }
+
+    /**
      * @Then the instance should have one new :eventName event
      */
     public function theInstanceShouldHaveOneNewEvent($eventName)
     {
-        $eventNames = array_map(function (Event $event) {
-            return $event->getName();
-        }, $this->instance->getNewEvents());
+        $events = $this->instance->getNewEvents();
 
-        expect($eventNames)->toContain($eventName);
+        expect($events)->toHaveCount(1);
+        expect($events[0]->getName())->toBe($eventName);
     }
 
     /**
-     * @Then the event should have a new UID
+     * @Then the method :arg1 should be called on the wrapped instance
      */
-    public function theEventShouldHaveANewUid()
+    public function theMethodShouldBeCalledOnTheWrappedInstance($arg1)
     {
-        throw new PendingException();
+        expect($this->instance->getCalls('testAction'))->toBe(1);
+    }
+
+    /**
+     * @Then the instance should have a new :eventName event
+     */
+    public function theInstanceShouldHaveANewEvent($eventName)
+    {
+        $events = $this->instance->getNewEvents();
+
+        $event = array_pop($events);
+        expect($event->getName())->toBe($eventName);
     }
 
     /**
--- features/creating-event-entities.feature
+++ features/creating-event-entities.feature
@@ -7,4 +7,10 @@ Feature: Creating Event Entity
     Given there is an entity class named TestEntity
     When I create an instance with the factory for TestEntity
     Then the instance should have one new Create event
-    And the event should have a new UID
+
+  Scenario: Calling a wrapped method
+    Given there is an entity class named TestEntity with a method called testAction
+    And I have created an instance of TestEntity with the factory
+    When I call "testAction" on then instance
+    Then the method "testAction" should be called on the wrapped instance
+    And the instance should have a new "testAction" event
